372382806d0248a3c558da99a1aadeeb9fdcbd6f
DorianBajorek <dorianb324@gmail.com>
2023-03-15 20:54:02 +0100

    Add dropdown with languages

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,19 +1,47 @@
-const ReactPrismEditor = require('react-prism-editor');
+
+import './About.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import { useState } from 'react';
 
 const code = `
 console.log("hello world");
 `;
+enum Language {
+  CPP = "C++",
+  PYTHON = "Python", 
+  JAVA = "Java"
+}
+
+const mapOfLanguages = new Map<Language, string>()
+    .set(Language.CPP, "cpp")
+    .set(Language.JAVA, "java")
+    .set(Language.PYTHON, "python");
 
 export default function About() {
-  return <ReactPrismEditor
-    language='javascript'
-    theme='tomorrow'
-    code={code}
-    lineNumber
-    readOnly={false}
-    clipboard={true}
-    changeCode={(code: any) => {
-      console.log(code);
-    }}
-/>;
+  const [language, setLanguage] = useState<Language>(Language.CPP); 
+  return <div className="editor-container">
+    <div>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          {language}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {Object.values(Language).map(value => <Dropdown.Item key={value} onClick={() => setLanguage(value)}>{value}</Dropdown.Item>)}
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+    <div className="editor-field"><CodeEditor
+  value={code}
+  language={mapOfLanguages.get(language)}
+  placeholder={`Please enter ${language} code.`}
+  onChange={(evn) => {}}
+  padding={15}
+  data-color-mode="dark"
+  style={{
+    fontSize: 15,
+    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+  }}
+/></div></div>;
 }
