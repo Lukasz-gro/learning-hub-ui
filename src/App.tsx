0b09310ac9037d9faceac9a5999ab4d6986b1b27
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-04-02 00:18:30 +0200

    Test backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import { healthCheck } from './services/learning-hub';
 import Homepage from './views/homepage';
 import CodeEditor from './views/codeEditor';
 import NavHeader from './navigation/NavHeader';
@@ -9,12 +7,6 @@ import About from './views/about/About';
 
 
 function App() {
-  const [serverResponse, setServerResponse] = useState<string>('No request made')
-
-  const talkWithServer = () => {
-    healthCheck().then(setServerResponse).catch(console.log);
-  };
-
   return (
     <BrowserRouter>
       <div className="App">
