e8d49de3fa49e7287bfd1335bc74e95142977960
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-04-02 18:48:02 +0200

    Change url in request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Homepage from './views/homepage';
 import CodeEditor from './views/codeEditor';
@@ -7,21 +7,20 @@ import About from './views/about/About';
 
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Homepage />}])
   return (
-    <RouterProvider router={router} /> );
-      {/* <div className="App">
+    <BrowserRouter>
+      <div className="App">
         <NavHeader />
-        <header className="App-header"> */}
-          {/* <Routes>
+        <header className="App-header">
+          <Routes>
             <Route index element={<Homepage/>}/>
             <Route path='code-editor' element={<CodeEditor/>}/>
             <Route path='about' element={<About/>}/>
-          </Routes> */}
-        {/* </header>
-      </div> */}
-    // </RouterProvider>
-  // );
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 
 export async function healthChecks(): Promise<string> {
-    return (await axios.get('/v1/health-check')).data as string;
+    return (await axios.get('http://13.48.78.45:7788/v1/health-check')).data as string;
+    // return (await axios.get('http://localhost:8080/v1/health-check')).data as string;
   }
 
 export default function Homepage() {
