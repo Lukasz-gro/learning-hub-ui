22d2b6e11e5e04d737e601ce66afc406c9375581
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-04-16 13:30:32 +0200

    Add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port : 5173,
+    proxy: {
+        '^/v1/.*': {
+            target: 'http://16.16.104.160',
+            changeOrigin: true,
+            secure: false
+        }
+    }
+},
 })
