99f89170054028c051b3703f6d9a1548c8038b95
DorianBajorek <dorianb324@gmail.com>
2023-05-27 16:06:36 +0200

    Add label for chatBot

--- src/views/chatBot/QuestionForBotLabel.tsx
+++ src/views/chatBot/QuestionForBotLabel.tsx
@@ -3,12 +3,17 @@ import './Label.css';
 export default function QuestionForBotLabel() {
     return(
         <div className="box-container">
-            <div className="question"></div>
-            <div className="question"></div>
-            <div className="question"></div>
-            <div className="question"></div>
-            <div className="question"></div>
-            <div className="question"></div>
+            <div className="question-list">
+                <div className="question">How are you?</div>
+                <div className="question">Give me a hint!</div>
+                <div className="question">Where is a mistake?</div>
+                <div className="question">Explain me exercise!</div>
+                <div className="question"></div>
+                <div className="question"></div>
+                <div className="question"></div>
+                <div className="question"></div>
+                <div className="question"></div>
+            </div>
         </div>
     )
 }
